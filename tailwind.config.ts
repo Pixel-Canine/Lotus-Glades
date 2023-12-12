8010ec55733ecb593ff295e6929b8b8d7b841018
Siddharth Borman <siddharth02b@gmail.com>
2023-12-13 02:09:02 +0530

    added colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        LotusBeige : '#EFEAE7',
+        LotusPink : '#951D51',
+        LotusBlush : '#F8ECDD',
+        LotusGreen : '#DCE9DE',
+        LotusBlack : '#000000',
+      }
     },
   },
   plugins: [],

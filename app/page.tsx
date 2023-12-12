4c3fffbb6803684b704423875d2d4f6fb8a7aa67
Siddharth Borman <siddharth02b@gmail.com>
2023-12-13 02:13:41 +0530

    added home skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -2,112 +2,156 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='body bg-[#EFEAE7]'>
+      {/* nav starts */}
+      {/* nav ends */}
+
+      {/*hero starts*/}
+      <div className='hero w-full px-[20px] h-screen flex flex-col'>
+        <div className='hero_img flex-grow bg-slate-200'>
+          {/* insert image from cms */}
+        </div>
+        <div className='hero_text mt-[32px]'>
+          <div style={{fontFamily : 'ClearfaceStd , sans-serif', fontWeight : 'bold', fontSize : '48px', letterSpacing : '-3%', lineHeight: '58px'}} className='hero_text-title'>
+            A Mindset of Abundance
+          </div>
+          <div style={{fontFamily: 'Apercu, sans-serif', lineHeight: '140%'}} className='hero_text-body mt-[16px]'>
+            Lotus Glades offers innovative opportunities for clients in Delhi's L-Zone as per our understanding of Delhi Masterplan 2041. With expertise in Land & Farmhouse, development, we provide high-end luxury living complying with the GDA policy and LDRA approved Farmhouses. Our land parcels in Jhatikra & Kanganheri (Delhi Pincode 110043) present an exceptional early-stage investment opportunity in approved Farmhouses.
+          </div>
         </div>
       </div>
+      {/* hero ends */}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='border-t-2 border-black my-[64px] mx-[20px]'></div>
+
+      {/* featured starts here */}
+      <div className='featured w-full px-[20px]'>
+        <div className='featured_text'>
+          <div style={{fontFamily : 'ClearfaceStd , sans-serif', fontWeight : 'bold', fontSize : '48px', letterSpacing : '-3%', lineHeight: '58px'}} className='featured_text-title'>
+            Featured Projects
+          </div>
+          <div style={{fontFamily: 'Apercu, sans-serif', lineHeight: '140%'}} className='featured_text-body mt-[16px]'>
+          Escape to charm and comfort in our featured farmhouses. Discover local treasures, embrace the simple life, and create memories that last a lifetime.
+          </div>
+        </div>
+        <div className='mt-[32px]'>
+          <div className='h-[193.5px] w-[350px] bg-slate-300'>
+          </div>
+        </div>
+       
       </div>
+      {/* featured ends here */}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className='border-t-2 border-black my-[64px] mx-[20px]'></div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      {/* case study starts */}
+      <div className='CaseStudy px-[20px]'>
+        <div className='CaseStudy_card'>
+          <div className='CaseStudy_card_image h-[261px] w-[350px] bg-blue-200'></div>
+          <div className='CaseStudy_card_text mt-[32px]'>
+            <div style={{fontFamily : 'ClearfaceStd , sans-serif', fontWeight : 'bold', fontSize : '48px', letterSpacing : '-3%', lineHeight: '58px'}} className='CaseStudy_card_text-title'>
+              The Sunken Courtyard
+            </div>
+            <div style={{fontFamily: 'Apercu, sans-serif', lineHeight: '140%'}} className='CaseStudy_card_text-title mt-[16px]'>
+            The Sunken Courtyard Farmhouse is a unique property located in a quiet village on the outskirts of Delhi. It features a traditional courtyard design with modern elements, offering a tranquil haven amidst the bustling city.
+            </div>
+          </div>
+          <div className='border-t-2 border-black my-[16px]'></div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <div className='CaseStudy_card_links w-full flex flex-row justify-evenly'>
+            <div style={{fontFamily: 'Apercu, sans-serif', fontSize : '16px' }} className='CaseStudy_card_links-link1'><a href='#'>Link1</a></div>
+            <div style={{fontFamily: 'Apercu, sans-serif', fontSize : '16px' }} className='CaseStudy_card_links-link2'><a href='#'>Link2</a></div>
+          </div>
+        </div>
+        <div className='CaseStudy_dots w-full flex flex-row justify-center my-[16px]'>
+          <div className='CaseStudy_dots-dot1 h-[20px] w-[20px] bg-black rounded-xl mr-[8px]'></div>
+          <div className='CaseStudy_dots-dot2 h-[20px] w-[20px] rounded-xl border-2 border-black mr-[8px]'></div>
+          <div className='CaseStudy_dots-dot3 h-[20px] w-[20px] rounded-xl border-2 border-black'></div>
+        </div>
       </div>
-    </main>
+      {/* case study ends here */}
+
+      <div className='border-t-2 border-black my-[64px] mx-[20px]'></div>
+
+      {/* services start here */}
+      <div  className='Services w-full px-[20px]'>
+        <div style={{fontFamily : 'ClearfaceStd , sans-serif', fontWeight : 'bold', fontSize : '48px', letterSpacing : '-3%', lineHeight: '58px'}} className='Services_title text-center'>Our Services</div>
+        <div className='Services_list mt-[32px] flex flex-col justify-center'>
+          <div className='Services_list-1 flex flex-col justify-center'>
+            <div className='Services_list-1_text flex flex-col justify-center'>
+              <div style={{fontFamily: 'ClearfaceStd, sans-serif', fontSize: '28px', lineHeight: '58px' , letterSpacing : '-3%'}} className='Services_list-1_text-title text-center'>Sale & Purchase</div>
+              <div style={{fontFamily: 'Apercu, sans-serif', fontSize : '16px', lineHeight : '140%' }} className='Sertext_list-1_text-body text-center'>Buy or sell your dream farmhouse with ease.
+                  Expert agents, comprehensive services, personalized approach.
+                  Market analysis, financing, legal support, and more.
+                  Let's find your perfect farm escape.</div>
+            </div>
+          </div>
+
+          <div className='Services_list-2 flex flex-col justify-center'>
+            <div className='Services_list-2_text flex flex-col justify-center'>
+              <div style={{fontFamily: 'ClearfaceStd, sans-serif', fontSize: '28px', lineHeight: '58px' , letterSpacing : '-3%'}} className='Services_list-1_text-title text-center'>Development & Construction</div>
+              <div style={{fontFamily: 'Apercu, sans-serif', fontSize : '16px', lineHeight : '140%' }} className='Sertext_list-1_text-body text-center'>Experienced team, expert planning, sustainable solutions.
+              Turnkey services, high-quality construction, timely completion.
+              From concept to reality, your farmhouse awaits.</div>
+            </div>
+          </div>
+
+          <div className='Services_list-2 flex flex-col justify-center'>
+            <div className='Services_list-2_text flex flex-col justify-center'>
+              <div style={{fontFamily: 'ClearfaceStd, sans-serif', fontSize: '28px', lineHeight: '58px' , letterSpacing : '-3%'}} className='Services_list-1_text-title text-center'>Consulting & Management</div>
+              <div style={{fontFamily: 'Apercu, sans-serif', fontSize : '16px', lineHeight : '140%' }} className='Sertext_list-1_text-body text-center'>Effortless living, worry-free enjoyment.
+              Maintenance, landscaping, staff, security.
+              Relax and unwind, your haven awaits.</div>
+            </div>
+          </div>          
+        </div>
+      </div>
+      {/* services end here */}
+
+      {/* footer starts here */}
+      <div className='footer mt-64 bg-LotusPink w-full text-[#EFEAE7]'>
+
+        <div className='footer_container my-32  px-[20px]'>
+          
+          <div style={{fontFamily: 'ClearfaceStd, sans-serif', fontSize: '28px', lineHeight: '58px' , letterSpacing : '-3%'}} className='fotter_container-title text-[#EFEAE7] text-center'>Lotus Glades</div>
+          
+          <div style={{fontFamily: 'Apercu, sans-serif', fontSize : '16px' , lineHeight : '140%'}} className='footer_container_body mt-[24px] flex flex-row justify-between'>
+            
+            <div className='footer_container_body-links'>
+              <div className='footer_container_body-links-home mb-[8px]'><a href='#'>Home</a></div>
+              <div className='footer_container_body-links-about mb-[8px]'><a href='#'>About</a></div>
+              <div className='footer_container_body-links-projects mb-[8px]'><a href='#'>Projects</a></div>
+              <div className='footer_container_body-links-blog'><a href='#'>Blog</a></div>
+            </div>
+            
+            <div className='footer_container-body-contacts'>
+              <div className='footer_container-body-contacts-address mb-[8px]'>
+                A-9, Khasra No. 371, 3rd Floor,<br/> Near Shiv Murti, Rangpuri,<br/> New Delhi - 110037
+              </div>
+              <div className='footer_container-body-contacts-number mb-[8px]'>
+                +91 70421 10100
+              </div>
+              <div className='footer_container-body-contacts-mail mb-[8px]'>
+                info@lotusglades.com
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='footer_container2 mt-[64px] pb-[32px]'>
+          <div className='footer_container2_socials'>
+            <a href='#'>
+            <div className='footer_container2_socials-fb'>
+              <div className='footer_container2_socials-fb-icon'>
+              </div>
+              <div style={{fontFamily : 'Apercu, sans-serif' , fontSize : '12px', lineHeight : '140%'}} className='footer_container2_socials-fb-name'>Facebook</div>
+            </div>
+            </a>
+          </div>
+          <div>copyright</div>
+        </div>
+      </div>
+      {/* footer ends here */}
+
+      </div>
   )
 }
